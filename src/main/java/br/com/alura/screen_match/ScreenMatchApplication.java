fda1f4b337d6b85e7b749220c6b55de5afe3c32b
david santos <david.sobral.profissional645@gmail.com>
2024-07-04 01:01:55 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screen_match/ScreenMatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screen_match;
+
+import br.com.alura.screen_match.model.DadosSerie;
+import br.com.alura.screen_match.services.ConsumoAPI;
+import br.com.alura.screen_match.services.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=dark&apikey=c9130ef2");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
